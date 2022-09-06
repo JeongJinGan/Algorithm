4d382cbd5f30cd10edf2c4de4a380d59e163ed5d
JeongJinGan <rksaudwls123@naver.com>
2022-09-06 16:30:44 +0900

    update BOJ

--- Baekjoon/10815_숫자_카드/10815_숫자_카드.py
+++ Baekjoon/10815_숫자_카드/10815_숫자_카드.py
@@ -3,8 +3,9 @@ import sys
 sys.stdin = open("10815.txt", "r")
 
 M = int(sys.stdin.readline())
-nums = list(map(int, sys.stdin.readline().split()))
-
+# set()을 안하면 시간초과가 뜬다.
+# nums = list(map(int, sys.stdin.readline().split()))
+nums = set(map(int, sys.stdin.readline().split()))
 N = int(sys.stdin.readline())
 nums2 = list(map(int, sys.stdin.readline().split()))
 
@@ -14,6 +15,8 @@ for i in nums2:
         print(1, end=" ")
     else:
         print(0, end=" ")
+
+# 리스트를 새로 선언해서 비교한다음 1,0을 알맞게 넣는 방법으로 풀어가려했다
 # list_ = [0] * N
 # k = 0       
 # for i in nums2:
